b727c8380790974b61beaf1a8a6f73a9c298af7c
tomasdarienko <darienkotomas@gmail.com>
2024-01-10 17:38:47 +0100

    added jumbo  and comics

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,46 @@
+<script>
+
+
+
+export default {
+
+     data() {
+          return {
+
+          }
+     }
+}
+
+</script>
+
+<template lang="">
+          
+          <div class="jumbo">
+               <button>CORRENT SERIES</button>
+          </div>
+        
+</template> 
+
+<style lang="scss" scoped> 
+ @use '../styles/general.scss' as*;
+ @use '../styles/partials/variables.scss' as*;
+
+ .jumbo{
+         background-image: url('../../public/img/jumbotron.jpg');
+         background-size: cover;
+         background-repeat: no-repeat;
+         height: 500px;
+         position: relative;
+         margin-bottom: 40px;
+
+         button{
+          color: white;
+          padding: 20px;
+          background-color: $colore_blu;
+          position: absolute;
+          left: 200px;
+          bottom:-30px
+         }
+      }
+ 
+</style>
---
+++ src/components/SingleComic.vue
@@ -0,0 +1,47 @@
+<script>
+
+
+
+export default {
+
+     props: {
+          fumetto: Object
+     },
+     data() {
+          return {
+
+          }
+     }
+}
+
+</script>
+
+<template lang="">
+          
+                <div class="carta">
+                    <img :src="fumetto.thumb" />
+                    <h4>{{fumetto.series}}</h4>
+                </div>
+        
+</template> 
+
+<style lang="scss" scoped> @use '../styles/general.scss' as*;
+ @use '../styles/partials/variables.scss' as*;
+
+
+ .carta {
+
+      width: calc(100% / 6 - 20px);
+      margin: 30px 10px;
+      height: 200px;
+
+      img {
+           height: 200px;
+           width: 200px;
+           object-fit: cover;
+      }
+      h4{
+          text-transform: uppercase;
+      }
+ }
+</style>
